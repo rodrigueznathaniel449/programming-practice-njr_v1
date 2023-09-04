34aadcc239e8c45db36c4e1d4d9a957e2376a22a
rodrigueznathaniel449 <rodrigueznathaniel449@gmail.com>
2023-09-04 14:40:55 -0400

    js commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//creation of script.js
+//creation of calculation portion of calculator
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value)
+{
+    if (isNaN(value));
+    {
+        handleSymbol(value);
+    }
+    else 
+    {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
