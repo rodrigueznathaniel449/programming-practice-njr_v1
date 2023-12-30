c81d71a8897c1b5a5ef6177a8a2c837c86f10b66
rodrigueznathaniel449 <rodrigueznathaniel449@gmail.com>
2023-12-29 22:53:21 -0500

    Docker Init, and Dependency Scoping

--- strainchain/app.py
+++ strainchain/app.py
@@ -19,56 +19,51 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/")
-@login_required
+@app.route("/",  methods=["GET", "POST"])
 def index():
     """Show the Home Page"""
     return render_template("index.html")
 
-@app.route("/about")
-@login_required
+@app.route("/about",  methods=["GET", "POST"])
 def about():
     """Show the About Us Page"""
     return render_template("about.html")
 
-@app.route("/learn")
-@login_required
+@app.route("/learn",  methods=["GET", "POST"])
 def learn():
     """Show the Learn Page"""
     return render_template("learn.html")
 
-@app.route("/budtender")
-@login_required
+@app.route("/budtender",  methods=["GET", "POST"])
 def budtender():
     """Show the BudTender Page"""
     return render_template("budtender.html")
 
-@app.route("/library")
-@login_required
+@app.route("/library",  methods=["GET", "POST"])
 def library():
     """Show the Library Page"""
     return render_template("library.html")
 
-@app.route("/rts")
-@login_required
+@app.route("/rts",  methods=["GET", "POST"])
 def rts():
     """Show the Real-Time Strategy Page"""
     return render_template("rts.html")
 
-@app.route("/myaccount")
+@app.route("/myaccount",  methods=["GET", "POST"])
 @login_required
 def myaccount():
     """Show the My Account Page"""
     return render_template("myaccount.html")
 
-@app.route("/login")
-@login_required
+@app.route("/login",  methods=["GET", "POST"])
 def index():
     """Show the Login Page"""
     return render_template("index.html")
 
-@app.route("/register")
-@login_required
+@app.route("/register",  methods=["GET", "POST"])
 def index():
     """Show the Registration Page"""
     return render_template("register.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
