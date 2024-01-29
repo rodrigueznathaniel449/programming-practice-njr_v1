9885a28a43af695d658efd2b03930a9fc7608a66
rodrigueznathaniel449 <rodrigueznathaniel449@gmail.com>
2024-01-28 20:04:44 -0500

    narrow scope of JS query to page, rename JS to regular script, reseed in layout over html direct

---
+++ strainchain/static/scripts/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.location.pathname === '/networks-build.html') {
+        document.getElementById('next1').addEventListener('click', function() {
+            console.log('Next1 button clicked')
+            document.getElementById('step1').classList.add('d-none');
+            document.getElementById('step2').classList.remove('d-none');
+        });
+        
+        document.getElementById('prev2').addEventListener('click', function() {
+            console.log('Prev2 button clicked')
+            document.getElementById('step2').classList.add('d-none');
+            document.getElementById('step1').classList.remove('d-none');
+        });
+        
+        document.getElementById('next2').addEventListener('click', function() {
+            console.log('Next2 button clicked')
+            document.getElementById('step2').classList.add('d-none');
+            document.getElementById('step3').classList.remove('d-none');
+        });
+        
+        document.getElementById('prev3').addEventListener('click', function() {
+            console.log('Prev3 button clicked')
+            document.getElementById('step3').classList.add('d-none');
+            document.getElementById('step2').classList.remove('d-none');
+        });
+    }
+});
+
