9c9ad05845e27d62b6d504aa89ed56c497b814a6
rodrigueznathaniel449 <rodrigueznathaniel449@gmail.com>
2024-01-29 18:40:45 -0500

    idek man

--- strainchain/static/scripts/script.js
+++ strainchain/static/scripts/script.js
@@ -1,33 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.pathname === '/networks-build') {
-        console.log('On networks-build page')
-        document.getElementById('next1').addEventListener('click', function(e) {
-            e.preventDefault();
-            console.log('Next1 button clicked')
-            document.getElementById('step1').classList.add('d-none');
-            document.getElementById('step2').classList.remove('d-none');
-        });
-        
-        document.getElementById('prev2').addEventListener('click', function(e) {
-            e.preventDefault();
-            console.log('Prev2 button clicked')
-            document.getElementById('step2').classList.add('d-none');
-            document.getElementById('step1').classList.remove('d-none');
-        });
-        
-        document.getElementById('next2').addEventListener('click', function(e) {
-            e.preventDefault();
-            console.log('Next2 button clicked')
-            document.getElementById('step2').classList.add('d-none');
-            document.getElementById('step3').classList.remove('d-none');
-        });
-        
-        document.getElementById('prev3').addEventListener('click', function(e) {
-            e.preventDefault();
-            console.log('Prev3 button clicked')
-            document.getElementById('step3').classList.add('d-none');
-            document.getElementById('step2').classList.remove('d-none');
-        });
-    }
-});
+let currentStep = 1;
 
+        function nextStep(step) {
+            // Hide the current step
+            document.getElementById(`step${currentStep}`).style.display = 'none';
+
+            // Show the next step
+            currentStep = step;
+            document.getElementById(`step${currentStep}`).style.display = 'block';
+        }
