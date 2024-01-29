e1a2bfe0974715aac17b13fd556cba404918c199
rodrigueznathaniel449 <rodrigueznathaniel449@gmail.com>
2024-01-28 21:41:46 -0500

    trying to go back in time lol

--- strainchain/static/scripts/script.js
+++ strainchain/static/scripts/script.js
@@ -1,28 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     if (window.location.pathname === '/networks-build') {
         document.getElementById('next1').addEventListener('click', function(e) {
-            e.preventDefault();
+            e.preventDefault(); // Prevent form submission
             document.getElementById('step1').classList.add('d-none');
             document.getElementById('step2').classList.remove('d-none');
         });
         
         document.getElementById('prev2').addEventListener('click', function(e) {
-            e.preventDefault();
+            e.preventDefault(); // Prevent form submission
             document.getElementById('step2').classList.add('d-none');
             document.getElementById('step1').classList.remove('d-none');
         });
         
         document.getElementById('next2').addEventListener('click', function(e) {
-            e.preventDefault();
+            e.preventDefault(); // Prevent form submission
             document.getElementById('step2').classList.add('d-none');
             document.getElementById('step3').classList.remove('d-none');
         });
         
         document.getElementById('prev3').addEventListener('click', function(e) {
-            e.preventDefault();
+            e.preventDefault(); // Prevent form submission
             document.getElementById('step3').classList.add('d-none');
             document.getElementById('step2').classList.remove('d-none');
         });
     }
 });
-
